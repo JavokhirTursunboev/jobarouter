a10291278a4d5a629423f9b9e861cb64f6493a64
Javokhir <jtursunboev@bk.ru>
2023-10-21 19:21:38 +0900

    breadcumbs map is done

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,16 +1,18 @@
 import { NavLink, Outlet } from "react-router-dom";
+import Breadcrumbs from "../components/Breadcrumbs";
 
 function Layout() {
   return (
     <div className="layout">
       <header>
         <nav>
-          <h1>JonaRounter</h1>
+          <h1>JobaRouter</h1>
           <NavLink to="/">Home </NavLink>
           <NavLink to="/about">About</NavLink>
           <NavLink to="/help">Help</NavLink>
           <NavLink to="careers">Careers</NavLink>
         </nav>
+        <Breadcrumbs />
       </header>
       <main>
         <Outlet />
